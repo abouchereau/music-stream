4e7337acf03f5bbad6058dcefe03e771cf8c1aeb
abouchereau <anthonybouchereau@hotmail.com>
2025-11-05 11:07:43 +0100

    Update service worker registration version to 0.4

--- public/js/front.js
+++ public/js/front.js
@@ -5,7 +5,7 @@
     } 
  
  if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js?v=0.3',{scope: '/'+playlistDir})
+      navigator.serviceWorker.register('/service-worker.js?v=0.4',{scope: '/'+playlistDir})
         .then(reg => console.log('SW registered', reg))
         .catch(err => console.error('SW reg failed', err));
     }
