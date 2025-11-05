970f745e1a24eb2b8058b18ea65af44fe4db9b52
abouchereau <anthonybouchereau@hotmail.com>
2025-11-05 14:40:26 +0100

    Update service worker to ignore API requests
    Ignore requests that start with '/api/' in the service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,10 @@ self.addEventListener('fetch', event => {
   //console.log("SW FETCH")
   const req = event.request;
   const url = new URL(req.url);
-    //console.log("URL",url.pathname);
+  
+ // Ignore les requêtes qui partent déjà vers /api/
+  if (url.pathname.startsWith('/api/')) return;
+  
   if (url.pathname.startsWith('/proxy/stream')) {  
     event.respondWith(handleProtectedAudio(req));
   }
