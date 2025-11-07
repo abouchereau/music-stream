3eddc4b9c081e0eff85fccb6d8dae558e72c2054
abouchereau <anthonybouchereau@hotmail.com>
2025-11-07 13:05:16 +0100

    Update front.js

--- public/js/front.js
+++ public/js/front.js
@@ -5,7 +5,7 @@ let playlistDir = window.location.pathname.slice(1);
     } 
  
  if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js?v=0.16',{scope: '/'+playlistDir})
+      navigator.serviceWorker.register('/service-worker.js?v=0.17',{scope: '/'+playlistDir})
         .catch(err => console.error('SW reg failed', err));
     }
     const API_URL = "/api"; 
@@ -27,15 +27,10 @@ let playlistDir = window.location.pathname.slice(1);
       const curLi = Array.from(items).find(li => li.dataset.index==index)
       curLi.classList.add('active');
 
-        const urlAudio = PROXY_URL+"/stream/"+playlistDir+"/"+index;
+    const urlAudio = PROXY_URL+"/stream/"+playlistDir+"/"+index;
       player.source = {
         type: 'audio',
-        sources: [
-          {
-            src: urlAudio,
-            type: 'audio/mpeg'
-          }
-        ]
+        sources: [{src: urlAudio,  type: 'audio/mpeg'}]
       };
       current = index;
         if (index != 0 || forcePlay) {
@@ -84,18 +79,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
-    player.on('ended', () => {    
-      const next = (current + 1);        
-        if (next > 0 && next < tracks.length) {
-          loadTrack(next);
-        }
-    });
-
-    function formatDuration(sec) {
-      if (!sec) return '';
-      const m = Math.floor(sec / 60);
-      const s = Math.floor(sec % 60);
-      return `${m}:${s.toString().padStart(2, '0')}`;
+player.on('ended', () => {
+  const next = (current + 1);        
+    if (next > 0 && next < tracks.length) {
+      loadTrack(next);
     }
+});
+
+function formatDuration(sec) {
+  if (!sec) return '';
+  const m = Math.floor(sec / 60);
+  const s = Math.floor(sec % 60);
+  return `${m}:${s.toString().padStart(2, '0')}`;
+}
 
 
