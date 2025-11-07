de273b25de4f24206aeca60955cc0c23373a067c
abouchereau <anthonybouchereau@hotmail.com>
2025-11-07 13:21:28 +0100

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,7 @@ self.addEventListener('activate', async event => {
 self.addEventListener('fetch',async event => {
   const req = event.request;
   const url = new URL(req.url);
- /* const client = event.clientId ? await self.clients.get(event.clientId) : null;
+  const client = event.clientId ? await self.clients.get(event.clientId) : null;
 
   if (!client) {
     return new Response('Forbidden', {status: 403, headers: {'Cache-Control': 'no-store'}});
@@ -18,7 +18,7 @@ self.addEventListener('fetch',async event => {
   const referrer = event.request.referrer || client.url || '';
   if (!referrer.startsWith('https://player.lasaugrenue.fr')) {
     return new Response('Forbidden', {status: 403, headers: {'Cache-Control': 'no-store'}});
-  }*/
+  }
 
   if (url.pathname.startsWith('/proxy/stream')) {  
     event.respondWith(handleProtectedAudio(req));
